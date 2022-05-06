5ea0c25c4af4af4564c0fc29063b51aaef3a67ab
Nguyễn Minh Tuấn <tuannm1610@gmail.com>
2022-05-06 12:30:42 +0700

    done

---
+++ src/main/java/com/codegym/security/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.codegym.security;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}12345").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}12345").roles("ADMIN");
+    } // Xác thực bộ nhớ với thông tin đăng nhập và vai trò người dùng
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/user**").hasRole("USER")
+                .and()
+                .authorizeRequests().antMatchers("/admin**").hasRole("ADMIN")
+                .and()
+                .formLogin()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+    } // Cấu hình bảo mật dựa trên web cho tất cả các HTTP request.
+}
